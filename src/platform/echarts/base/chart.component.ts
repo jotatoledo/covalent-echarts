870202d6868943447fc5d63aeff08bc5421a66b1
Jose Toledo <jotatoledon@gmail.com>
2018-11-09 23:46:42 +0100

    refactor: rework base chart component to use echart types

--- src/platform/echarts/base/chart.component.ts
+++ src/platform/echarts/base/chart.component.ts
@@ -11,11 +11,9 @@ import {
   OnDestroy,
   DoCheck,
 } from '@angular/core';
-
-import { Subscription, Subject, fromEvent, merge, timer } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-
-import * as echarts from 'echarts/lib/echarts';
+import { Subscription, Subject, fromEvent, merge } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+import { connect, init, ECharts, EChartOption } from 'echarts';
 
 import { TdChartOptionsService, CHART_PROVIDER } from './chart-options.service';
 import { assignDefined } from './utils';
@@ -28,19 +26,31 @@ import { assignDefined } from './utils';
   providers: [CHART_PROVIDER],
 })
 export class TdChartComponent implements AfterViewInit, OnChanges, DoCheck, OnDestroy {
-
+  private readonly defaultOptions: EChartOption = {
+    grid: {
+      show: true,
+      left: '20',
+      right: '20',
+      bottom: '10',
+      top: '10',
+      containLabel: true,
+      borderColor: '#FCFCFC',
+    },
+    xAxis: [{}],
+    yAxis: [{}],
+    series: [],
+  };
   private _subs: Subscription[] = [];
   private _widthSubject: Subject<number> = new Subject<number>();
   private _heightSubject: Subject<number> = new Subject<number>();
 
-  private _instance: any;
+  private _state: EChartOption = {};
+  private _options: any = {};
 
-  get instance(): any {
+  private _instance: ECharts;
+  get instance(): ECharts {
     return this._instance;
   }
-
-  private _state: any = {};
-  private _options: any = {};
 
   @Input('config') config: any = {};
 
@@ -56,7 +66,7 @@ export class TdChartComponent implements AfterViewInit, OnChanges, DoCheck, OnDe
   }
 
   ngAfterViewInit(): void {
-    this._instance = echarts.init(this._elementRef.nativeElement);
+    this._instance = init(this._elementRef.nativeElement);
     this._subs.push(
       fromEvent(this._instance, 'click').subscribe((params: any) => {
         this.click.next(params);
@@ -74,7 +84,7 @@ export class TdChartComponent implements AfterViewInit, OnChanges, DoCheck, OnDe
     );
     if (this.group) {
       this._instance.group = this.group;
-      echarts.connect(this.group);
+      connect(this.group);
       this._changeDetectorRef.markForCheck();
     }
     this._subs.push(
@@ -126,20 +136,8 @@ export class TdChartComponent implements AfterViewInit, OnChanges, DoCheck, OnDe
 
   render(): void {
     if (this._instance) {
-      this._instance.setOption(assignDefined(this._state, {
-        grid: {
-          show: true,
-          left: '20',
-          right: '20',
-          bottom: '10',
-          top: '10',
-          containLabel: true,
-          borderColor: '#FCFCFC',
-        },
-        xAxis : [{}], // throws error if its empty
-        yAxis : [{}], // throws error if its empty
-        series: [],
-      }, this.config ? this.config : {}, this._options), true);
+      const mergedOptions: EChartOption = assignDefined(this._state, this.defaultOptions, this.config || {}, this._options);
+      this._instance.setOption(mergedOptions, true);
       this._changeDetectorRef.markForCheck();
     }
   }
